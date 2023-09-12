16df0291a9915175c28120591c44ddbdf529f9bc
Ethan <144839622+jeanbaptisteethan@users.noreply.github.com>
2023-09-12 18:35:52 -0400

    Add files via upload

---
+++ Web-project/test-site/scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/tatsumaki.jpg") {
+    myImage.setAttribute("src", "images/Mini_Tatsumaki2.png");
+  } else {
+    myImage.setAttribute("src", "images/tatsumaki.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Tatsumaki is cool. Nice to meet you ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Tatsumaki is cool. Nice to meet you ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
+
